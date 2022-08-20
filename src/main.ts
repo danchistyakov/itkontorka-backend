e42d7955c7fa9743acd86f93c17a21cfe728da38
Daniel Chistyakov <4i.danila@gmail.com>
2022-08-20 16:02:43 +0300

    Port fixed

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/api/doc", app, document);
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 
 bootstrap();
